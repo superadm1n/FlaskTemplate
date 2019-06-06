407e1aa2ad22e4925e53fafe746eac85be0bf219
Superadm1n <kowalkyl@gmail.com>
2019-06-06 14:33:53 -0500

    Added ability for plugins to enforce route access control

--- app/plugins/Plugin.py
+++ app/plugins/Plugin.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
-
+from flask import Blueprint, abort
+from flask_login import current_user
+from functools import wraps
 
 class Plugin(Blueprint):
     '''
     This class represents a plugin object, It should be used when extending the system via plugins
     '''
-    def __init__(self, required_roles, *args, **kwargs):
+    def __init__(self, access_roles, *args, **kwargs):
         '''
 
         :param required_roles: The roles that will be used when restricting access to routes contained in the plugin
@@ -13,4 +14,23 @@ class Plugin(Blueprint):
         :param kwargs: Keyword arguments that are passed to the flask.Blueprint object
         '''
         super().__init__(*args, **kwargs)
-        self.required_roles = required_roles
+        self.access_roles = access_roles
+
+    def required_roles(*roles):
+        '''Custom function for checking if a user has the required rolls to access a resource.
+        :param roles:
+        :return:
+        '''
+
+        def wrapper(f):
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                try:
+                    if current_user.has_role(*roles) is False:
+                        abort(401)
+                except AttributeError:
+                    abort(401)
+                return f(*args, **kwargs)
+
+            return wrapped
+        return wrapper
